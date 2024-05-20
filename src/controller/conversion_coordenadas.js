4c6dda28756b81ede6a466398b05c20bfc80a806
mayracadena <mayitacadena@gmail.com>
2024-05-20 09:00:20 -0500

    creacion clase ctm12

--- src/controller/conversion_coordenadas.js
+++ src/controller/conversion_coordenadas.js
@@ -1,9 +1,10 @@
 
 const GRS80 = require('../class/GRS80');
+const CTM12 = require('../class/CTM12');
 //vamos a definir las variables a utilizar:
 const grs = new GRS80();
+const ctm = new CTM12();
 
-console.log(grs.a);
 
 
 
