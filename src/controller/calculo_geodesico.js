72266ec960d56b50d31d8caeeba56f69f5ade49b
Mayra Cadena <mayitacadena@gmail.com>
2024-06-12 18:32:16 -0500

    calculo geoedesico directo terminado y probado

--- src/controller/calculo_geodesico.js
+++ src/controller/calculo_geodesico.js
@@ -1,6 +1,7 @@
 const GRS80 = require("../class/GRS80");
 const grs = new GRS80();
-
+//el apartado de bowring esta supeditado a la aprobación
+//por ahora solo quedan los modulos sin uso
 //para este calculo se tomo las ecuaciones encontradas en libro de Richard Rapp
 //denominado curso geodesia, geodesia geometrica, volumn 1, principios basicos
 //junio 1988, universidad estatal de Ohio
@@ -78,13 +79,19 @@ function problema_directo_bowring(phi1, landa1, a12, s) {
   console.log("azimut 2-1: ", a21);
 }
 
-problema_directo_bowring(4, -73, 45, 100);
+
+//problema_directo_bowring(4, -73, 45, 100);
 
 //Problema geodesico directo de Vincenty
 //este es el algoritmo mas utilizado debidoa  su precisión
 //tomado del siguiente link: https://www.ngs.noaa.gov/PUBS_LIB/inverse.pdf
 
 function problema_directo_Vicenty(phi1, landa1, a12, s) {
+  //se convierten los datos de entrada de decimales a radianes para poder trabajar este procedimiento
+  phi1 = phi1*(Math.PI/180);
+  landa1 = landa1*(Math.PI/180);
+  a12 = a12*(Math.PI/180);
+
   //a12 es el mismo alpha1
 
   //latitud reducida
@@ -95,10 +102,10 @@ function problema_directo_Vicenty(phi1, landa1, a12, s) {
   //sigma → tan(sigma) = tan(U1)/cos(a12)
   var sigma1 = Math.atan(Math.tan(U1) / Math.cos(a12));
   //u^2 = cos^2(alpha)*(a^2-b^2)/b
-  var u2 =(Math.pow(Math.cos(alpha), 2)*(Math.pow(grs.a, 2) - Math.pow(grs.b, 2)))/Math.pow(grs.b, 2);
+  var u2 =(Math.pow(Math.cos(alpha), 2)*((Math.pow(grs.a, 2) - Math.pow(grs.b, 2)))/Math.pow(grs.b, 2));
 
   //constantes A
-  var A = 1 + (u2 / 16384) * (4096 + u2 * (-768 + u2 * (320 - 175 * u2)));
+  var A = 1 + ((u2 / 16384) * (4096 + u2 * (-768 + u2 * (320 - 175 * u2))));
   //constante B
   var B = (u2 / 1024) * (256 + u2 * (-128 + u2 * (320 - 175 * u2)));
 
@@ -111,13 +118,12 @@ function problema_directo_Vicenty(phi1, landa1, a12, s) {
   //Se debe iterar hasta que halla un cambio insignificante en sigma
   while (Math.abs(sigma - sigmaP) > limit) {
     //se declara este 2sigmaM como sigmaM
-    var sigmaM = 2 * sigma1 + sigma;
+    var sigmaM = (2 * sigma1) + sigma;
 
     //se va hallar deltaSigma por partes
 
     //cos(sigma)(-1+2*cos^2(2sigmaM))
-    var deltaSigma1 =
-      Math.cos(sigma) * (-1 + 2 * Math.pow(Math.cos(sigmaM), 2));
+    var deltaSigma1 = Math.cos(sigma) * (-1 + 2 * Math.pow(Math.cos(sigmaM), 2));
     //1/6*B*cos(2*sigmaM)
     var deltaSigma2 = (1 / 6) * B * Math.cos(sigmaM);
     //-3+4*sen^2sigma
@@ -130,6 +136,91 @@ function problema_directo_Vicenty(phi1, landa1, a12, s) {
     sigma = s / (grs.b * A) + deltaSigma;
   }
 //ahora procedemos a hallar phi2, tambien por partes
+  
+//parte 1
+//sen(U1)*cos(sigma)
+var phi_1=  Math.sin(U1)*Math.cos(sigma);
+//parte 2
+//cos(U1)*sen(sigma)*cos(a12)
+var phi_2 = Math.cos(U1)*Math.sin(sigma)*Math.cos(a12);
+//parte 3
+//sen(U1)*sen(sigma)
+var phi_3 = Math.sin(U1)*Math.sin(sigma);
+//parte 4
+//cos(U1)*cos(sigma)*cos(a12)
+var phi_4 = Math.cos(U1)*Math.cos(sigma)*Math.cos(a12);
+
+//aca se pondra la ecuación completa
+var phi2 = Math.atan2((phi_1+phi_2),((1-grs.f)*Math.sqrt(Math.pow(Math.sin(alpha),2)+Math.pow(phi_3-phi_4,2))));
+
+//procedemos a calcular landa tambien por partes
+
+//parte 1
+//sen(sigma)*sen(a12)
+var landa_1 = Math.sin(sigma)*Math.sin(a12);
+//parte 2
+//cos(U1)*cos(sigma)
+var landa_2 = Math.cos(U1)*Math.cos(sigma);
+//parte 3
+//sen(U1)*sen(sigma)*cos(a12)
+var landa_3 = Math.sin(U1)*Math.sin(sigma)*Math.cos(a12);
+
+//aca estará la ecuación completa
+var landa = Math.atan2(landa_1,(landa_2-landa_3));
+
+//hallaremos C con el fin de facilitar tambien las formulas para hallar el diferencial de landa L
+var C = (grs.f/16)*(Math.pow(Math.cos(alpha),2)*(4+(grs.f*(4-(3*Math.pow(Math.cos(alpha),2))))));
+
+//hallando la diferencial landa L
+//tambien se hara por partes para facilitar el proceso
+
+//cos(2sigmaM)+C*cos(sigma)
+var L1 = Math.cos(sigmaM)+(C*Math.cos(sigma));
+//-1+2*cos^2(2sigmaM)
+var L2 = -1+(2*Math.pow(Math.cos(sigmaM),2));
+
+//aca esta la ecuacion completa del diferencial
+var L = landa-((1-C)*grs.f*Math.sin(alpha)*(sigma+C*(Math.sin(sigma)*(L1*L2))));
+var landa2 = landa1+L;
+
+//calculo para hallar el azimut de 2 a 1
+var a21 = Math.atan((Math.sin(alpha)/((-Math.sin(U1)*Math.sin(sigma))+(Math.cos(U1)*Math.cos(sigma)*Math.cos(a12)))));
+
+//una vez culminado el procedimiento se procede a pasar de radianes a decimales 
+phi2 = phi2*(180/Math.PI);
+landa2 = landa2*(180/Math.PI);
+a21 = a21*(180/Math.PI);
+//condicional para el azimut
+
+/*
+result[2] = Math.atan2(sinAlpha, -tmp) * 57.29577951308232D;
+    if (result[2] < 0.0D)
+      result[2] = result[2] + 360.0D; 
+    result[2] = result[2] + 180.0D;
+    if (result[2] > 360.0D)
+      result[2] = result[2] - 360.0D; 
+    result[1] = longitude1 + l;
+    result[1] = result[1] * 57.29577951308232D;
+    return result;
+  }
+*/
+
+if(a21 < 0){
+  a21 = a21+360;
+}else if(a21 >360){
+  a21 = a21-360;
+}else{
+  a21 = a21 + 180;
+}
+
+
+
+console.log('latitud: ',phi2);
+console.log('longitud: ',landa2);
+console.log('azimut 21: ',a21);
+
 
 
 }
+
+problema_directo_Vicenty(4, -73, 15, 100);
