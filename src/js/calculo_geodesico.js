fd2e45227eaa915464d088e77d422fa6b26ec1aa
Mayra Cadena <48366025+mayracadena@users.noreply.github.com>
2024-06-26 21:35:13 -0500

    visualizacion calculo geodesico directo e inverso

--- src/js/calculo_geodesico.js
+++ src/js/calculo_geodesico.js
@@ -1,13 +1,25 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const radioButtons = document.querySelectorAll('input[type="radio"]');
+
+//aca escucho el submit cuando envio los datos del calc_inver_direct.ejs
+document.getElementById('cal_directo').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita que el formulario se envíe de la manera tradicional
+
+    // Capturamos latitud
+    var lat_gg = parseInt(document.getElementById('lat_gg_d').value);
+    var lat_mm = parseInt(document.getElementById('lat_mm_d').value);
+    var lat_ss =  parseFloat(document.getElementById('lat_ss_d').value);
+    var hemisferio = document.getElementById('hemisferio').value;
+    //capturamos longitud
+    var long_gg = parseInt(document.getElementById('long_gg_d').value);
+    var long_mm = parseInt(document.getElementById('long_mm_d').value);
+    var long_ss =  parseFloat(document.getElementById('long_ss_d').value);
+    //capturamos la distancia
+    var dist =  parseFloat(document.getElementById('dist_d').value);
+    //capturamos azimut de 1 a 2
+    var az_gg = parseInt(document.getElementById('az_gg_d').value);
+    var az_mm = parseInt(document.getElementById('az_mm_d').value);
+    var az_ss =  parseFloat(document.getElementById('az_ss_d').value);
+
+
     
-    radioButtons.forEach(radioButton => {
-      radioButton.addEventListener('click', (event) => {
-        const selectedOption = event.target.value;
-        console.log(`Opción seleccionada: ${selectedOption}`);
-        
-        // Aquí puedes realizar cualquier acción adicional basada en la opción seleccionada
-        // Por ejemplo, actualizar otra parte de la interfaz o enviar datos al servidor, etc.
-      });
-    });
-  });
+
+});
